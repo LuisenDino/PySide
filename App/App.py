9847e95ac5cae36b78a4ffea24a07ed40fc6c301
LuisenDino <luisendino02@gmail.com>
2022-03-14 20:16:32 -0500

    Last update

--- App/App.py
+++ App/App.py
@@ -1,49 +1,38 @@
 import sys
 import logging
-import Player
-import Configuration
-#from App import Player
-#from App import Configuration
+
+from App.ConfigGUI.MainFrame import MainFrame as Configuration
+from App.Player import Player as Player
 import os
-import tkinter as tk
-from screeninfo import get_monitors
+
+from PySide2.QtWidgets import QApplication, QSplashScreen
+from PySide2.QtCore import *
+from PySide2.QtGui import *
 
 logging.basicConfig(filename=os.path.expanduser('~')+"/.config/Ciel/C-Media_Player/logs/log.log", level=logging.DEBUG, format=("%(asctime)s:%(levelname)s:%(message)s"), datefmt="%m/%d/%Y %I:%M:%S %p")
 
-splash_root = tk.Tk(className="C-Media Player")
 
-splash_root.title("C-Media")
+root = QApplication()
 
-image = tk.PhotoImage(file=os.path.expanduser('~')+"/.config/Ciel/C-Media_Player/Media/LOGO-CMedia.png")
-splash_root.tk.call('wm', 'iconphoto', splash_root._w, image)
-monitors = get_monitors()
-for monitor in monitors:
-    if monitor.is_primary:
-        h=250
-        w=402
-        splash_root.geometry(str(w)+"x"+str(h)+"+"+str(monitor.width//2-w//2)+"+"+str(monitor.height//2-h//2)) 
-splash_root.configure(background="#f3f3f3")
-#splash_root.configure(background="#0a3749")
+pixmap = QPixmap(os.path.expanduser('~')+"/.config/Ciel/C-Media_Player/Media/SplashScreen.png")
+splash = QSplashScreen(pixmap)
+splash.show()
+root.processEvents()
+if len(sys.argv) == 1:
+    try:
+        player = Player()
+        splash.finish(player)
+        player.show()
+    except Exception as e:
+        logging.error(str(e))
+elif "--config" in sys.argv:
+    try:
+        config = Configuration()
+        splash.finish(config)
+        config.show()
+    except Exception as e:
+        logging.error(str(e))
+else:
+    logging.error("Opcion no permitida")
 
-img = tk.PhotoImage(file=os.path.expanduser('~')+"/.config/Ciel/C-Media_Player/Media/SplashScreen.png")
-#tk.Label(splash_root, image=img, bg="#0a3749").pack(fill=tk.BOTH, expand=True)
-tk.Label(splash_root, image=img, bg="#f3f3f3").pack(fill=tk.BOTH, expand=True)
-splash_root.overrideredirect(True)
-
-def call_app():
-    if len(sys.argv) == 1:
-        try:
-            Player.main(splash_root)
-        except Exception as e:
-            logging.error(str(e))
-    elif "--config" in sys.argv:
-        try:
-            Configuration.main(splash_root)
-        except Exception as e:
-            logging.error(str(e))
-    else:
-        logging.error("Opcion no permitida")
-    
-
-splash_root.after(2000, call_app)
-splash_root.mainloop()
+sys.exit(root.exec_())
