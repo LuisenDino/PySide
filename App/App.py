5a04df29b1a624fa8aa770abc4fd9d44d4070a60
Luis Enrique Carmona <66492831+LuisenDino@users.noreply.github.com>
2022-08-02 13:17:11 -0500

    Solved compilig problems

--- App/App.py
+++ App/App.py
@@ -3,23 +3,27 @@ from PySide2.QtWidgets import QApplication, QSplashScreen
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 
-
-#Clases internas - Compilación
-#Descomentar para realizar compilacion de la aplicacion
-from App.ConfigGUI.MainFrame import MainFrame as Configuration
-from App.Player import Player as Player
-"""
-
-#Clases internas - Pruebas 
-#Descomentar para realizar pruebas con python
-from ConfigGUI.MainFrame import MainFrame as Configuration
-from Player import Player as Player
-"""
-
 #Otras librerias 
 import sys
 import logging
 import os
+
+argv = sys.argv
+debug = "--debug" in sys.argv
+
+if debug:
+    #Clases internas - Pruebas 
+    #Descomentar para realizar pruebas con python
+    from ConfigGUI.MainFrame import MainFrame as Configuration
+    from Player import Player as Player
+    argv.remove("--debug")
+else:
+    #Clases internas - Compilación
+    #Descomentar para realizar compilacion de la aplicacion
+    from App.ConfigGUI.MainFrame import MainFrame as Configuration
+    from App.Player import Player as Player
+
+
 
 #Creacion del archivo de logs
 logging.basicConfig(filename=os.path.expanduser('~')+"/.config/Ciel/C-Media_Player/logs/log.log", level=logging.DEBUG, format=("%(asctime)s:%(levelname)s:%(message)s"), datefmt="%m/%d/%Y %I:%M:%S %p")
@@ -35,16 +39,17 @@ splash.show()
 root.processEvents()
 
 
+
 #Iniciacion
-if len(sys.argv) == 1:
+if len(argv) == 1:
     try:
-        player = Player()
+        player = Player(debug)
         splash.finish(player)
         player.show()
     except Exception as e:
         logging.error(str(e))
         sys.exit()
-elif "--config" in sys.argv:
+elif "--config" in argv:
     try:
         config = Configuration()
         splash.finish(config)
@@ -54,7 +59,7 @@ elif "--config" in sys.argv:
         sys.exit()
 else:
     logging.error("Opcion no permitida")
-
+    sys.exit()
 root.lastWindowClosed.connect(root.quit)
 #Fin de  la aplicacion
 sys.exit(root.exec_())
