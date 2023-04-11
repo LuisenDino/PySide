f91256a7d2c5a435cfa9c222d7ebe75d1ffdbfe0
Luis Enrique Carmona <66492831+LuisenDino@users.noreply.github.com>
2023-04-11 12:03:14 -0500

    Update WebTabFrame.py
    Changed Clicked Button color

--- App/ConfigGUI/WebTabFrame.py
+++ App/ConfigGUI/WebTabFrame.py
@@ -52,7 +52,7 @@ class WebTabFrame(QWidget):
         self.save_button = QPushButton("Guardar")
         self.save_button.clicked.connect(self.save_settings)
         self.mainLayout.addWidget(self.save_button, 3, 2, 1, 2)
-        self.save_button.setStyleSheet("background-color : rgb(88, 102, 108); padding: 6px; border-radius: 3px;color: white") # 58666c 
+        self.save_button.setStyleSheet("QPushButton{background-color : rgb(88, 102, 108); padding: 6px; border-radius: 3px;color: white}   QPushButton::pressed{background-color : rgb(62, 72, 77); padding: 6px; border-radius: 3px;color: white}") # 58666c 
 
         #Imagen 
         img =  QLabel()
